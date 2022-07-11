2d872d3b0c02b51cb7ece51f12289031b280f5d9
mohdabbas@078gmail.com <root@ip-172-31-92-58.ec2.internal>
2022-07-11 07:21:50 +0000

    changes from vir user mohd

--- main.java
+++ main.java
@@ -6,7 +6,7 @@ class M
  Scanner sc =new Scanner(System.in);
  int a=sc.nextInt();
  int b=sc.nextInt();
-int c= a*b;
+int c= a+b;// this code is perfect please don't make any changes
 Syatem.out.println("Result = "+c);
 }
 }

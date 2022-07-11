82c098b60b6d5e386752081ef8dc056dafed9409
mohdabbas@078gmail.com <root@ip-172-31-92-58.ec2.internal>
2022-07-11 06:58:28 +0000

    comit from mohd virginia user

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner ;
+class M
+{
+ public static void main(String arg[])
+{
+ Scanner sc =new Scanner(System.in);
+ int a=sc.nextInt();
+ int b=sc.nextInt();
+int c= a+b;
+Syatem.out.println("Result = "+c);
+}
+}
